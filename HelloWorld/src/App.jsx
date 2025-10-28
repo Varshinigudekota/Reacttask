be9dba2071e931ef3969a7a1ee8dddb16037cce7
Varshinigudekota <varshinigudekota55@gmail.com>
2025-10-28 16:05:29 +0530

    changes

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -13,7 +13,9 @@ function App() {
       <h1>I am from {place}</h1>
       <h3>Introduction: <br></br>My name is Varshini, and I am from Guntakal, Andhra Pradesh. I am a recent graduate with a degree in Computer Science. Throughout my academic journey, I have developed a strong interest in technology and software development. I am passionate about learning new concepts, exploring real-world applications, and continuously improving my technical and problem-solving abilities. I am eager to begin my professional career where I can apply my knowledge, gain practical experience, and grow both personally and professionally.</h3>
       <h3>Skills: <br></br>I have developed technical skills in Java, HTML, CSS, JavaScript, and MS Excel. I am comfortable working on web development projects using HTML, CSS, and JavaScript to create responsive and interactive designs. My knowledge of Java helps me in understanding object-oriented programming and backend logic. Additionally, I use MS Excel for data management, analysis, and visualization. I am always open to learning new tools and technologies to enhance my skill set.</h3>
-      
+      <h3>Languages: </h3><br></br>
+      <ol><li>English</li><li>Hindi</li><li>Telugu</li><li>Kannada</li></ol>
+      <h3>Images</h3>
       <img src="html.png"></img>
       <img src="css.png"></img>
       <img src="js.png"></img><br></br>
